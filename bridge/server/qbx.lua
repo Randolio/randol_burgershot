3b0af065e021158fa24ca6fae02458979d9f198e
Solareon <769465+solareon@users.noreply.github.com>
2024-08-28 13:50:01 +0200

    refactor(bridge): remove unused variable

--- bridge/server/qbx.lua
+++ bridge/server/qbx.lua
@@ -1,7 +1,6 @@
 if GetResourceState('qbx_core') ~= 'started' then return end
 
 local Config = lib.require('config')
-local ox_inv = GetResourceState('ox_inventory') == 'started'
 
 function GetPlayer(id)
     return exports.qbx_core:GetPlayer(id)
@@ -39,38 +38,36 @@ function isBurgerShot(Player)
     return Player.PlayerData.job.name == 'burgershot'
 end
 
-if ox_inv then
-    for i = 1, #Config.Zones do
-        local v = Config.Zones[i]
-        if v.type == 'stash' then
-            exports.ox_inventory:RegisterStash(v.stashLabel, v.stashLabel, v.slots, v.weight, false)
-        elseif v.type == 'shop' then
-            exports.ox_inventory:RegisterShop('Ingredients - Burgershot', {
-                name = 'Ingredients - Burgershot',
-                inventory = {
-                    { name = 'burger-bun', price = 0 },
-                    { name = 'burger-raw', price = 0 },
-                    { name = 'burger-lettuce', price = 0 },
-                    { name = 'burger-tomato', price = 0 },
-                    { name = 'burger-potato', price = 0 },
-                    { name = 'burger-mshakeformula', price = 0 },
-                    { name = 'burger-sodasyrup', price = 0 },
-                },
-            })
+for i = 1, #Config.Zones do
+    local v = Config.Zones[i]
+    if v.type == 'stash' then
+        exports.ox_inventory:RegisterStash(v.stashLabel, v.stashLabel, v.slots, v.weight, false)
+    elseif v.type == 'shop' then
+        exports.ox_inventory:RegisterShop('Ingredients - Burgershot', {
+            name = 'Ingredients - Burgershot',
+            inventory = {
+                { name = 'burger-bun', price = 0 },
+                { name = 'burger-raw', price = 0 },
+                { name = 'burger-lettuce', price = 0 },
+                { name = 'burger-tomato', price = 0 },
+                { name = 'burger-potato', price = 0 },
+                { name = 'burger-mshakeformula', price = 0 },
+                { name = 'burger-sodasyrup', price = 0 },
+            },
+        })
+    end
+end
+for k,v in pairs(Config.HungerFill) do
+    exports(k, function(event, item, inventory, slot, data)
+        if event == 'usingItem' then
+            TriggerClientEvent('randol_burgershot:client:Eat', inventory.id, k, slot, v.emote)
         end
-    end
-    for k,v in pairs(Config.HungerFill) do
-        exports(k, function(event, item, inventory, slot, data)
-            if event == 'usingItem' then
-                TriggerClientEvent('randol_burgershot:client:Eat', inventory.id, k, slot, v.emote)
-            end
-        end)
-    end
-    for k,v in pairs(Config.ThirstFill) do
-        exports(k, function(event, item, inventory, slot, data)
-            if event == 'usingItem' then
-                TriggerClientEvent('randol_burgershot:client:Drink', inventory.id, k, slot, v.emote)
-            end
-        end)
-    end
+    end)
+end
+for k,v in pairs(Config.ThirstFill) do
+    exports(k, function(event, item, inventory, slot, data)
+        if event == 'usingItem' then
+            TriggerClientEvent('randol_burgershot:client:Drink', inventory.id, k, slot, v.emote)
+        end
+    end)
 end
